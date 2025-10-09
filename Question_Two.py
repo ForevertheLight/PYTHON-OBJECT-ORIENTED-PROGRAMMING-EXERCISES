f3e94c4fa467f07d8380d70b116ea2462c273fdd
ForevertheLight <fashjosh2004@gmail.com>
2025-10-09 12:10:39 +0100

    Added Question_Two.py - Contains OOP Class Explanation and Examples

---
+++ Question_Two.py
@@ -0,0 +1,10 @@
+'''A class is a blueprint for creating objects.
+It defines what attributes (data) and methods (functions) the object will have.
+
+🔹 Example:'''
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+#This class 'Car' describes what every Car object will look like.

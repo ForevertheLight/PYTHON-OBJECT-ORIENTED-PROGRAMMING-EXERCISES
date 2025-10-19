f864a0ba5952e31f5c17dc2677f5a6b4ec07a696
ForevertheLight <fashjosh2004@gmail.com>
2025-10-19 04:12:49 +0100

    Added an explanation on Python Abstraction in OOP  with an example code

---
+++ Question_Eleven.py
@@ -0,0 +1,19 @@
+# Abstraction means hiding unnecessary details and showing only essential information.
+# In Python, it’s often achieved using abstract base classes (ABC).
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+sq = Square(4)
+print(sq.area())

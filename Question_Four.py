45cac75aedc273c124aa4df89040baec61b5e449
ForevertheLight <fashjosh2004@gmail.com>
2025-10-19 03:52:27 +0100

    Added a detailed explanation on python class constructor with an example code

---
+++ Question_Four.py
@@ -0,0 +1,6 @@
+#__init__ is a constructor method in Python. It runs automatically when a new object 
+#is created and initializes the object’s attributes.
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
